7a9d53f339c23d58df8639abba3c6d0978acb6f4
Pavel Rubis <rubisiay@gmail.com>
2020-06-14 22:26:57 +0300

    Коммит №1. Незаконченный макет главной и страницы заявки. Нужно не забыть сделать нормальную адаптацию для пк.

---
+++ jsFuncs.js
@@ -0,0 +1,26 @@
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+/* $(function() {
+	
+	$("nav select").change(function() {
+		window.location = $(this).find("option:selected").val();
+	});
+
+}); */
