4e87333d5b8f76bebd496de2dcebe3945c726a22
Pavel Rubis <rubisiay@gmail.com>
2020-06-30 01:38:15 +0300

    Коммит №5. Пытаюсь сделать нормальную шапку для мобил.

--- jsFuncs.js
+++ jsFuncs.js
@@ -32,3 +32,23 @@ function init(){
 	myMap.geoObjects.add(myPlacemark);
 		
 }
+
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Закройте выпадающее меню, если пользователь щелкает за его пределами
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn') && !event.target.matches('.dropbtnImg')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
